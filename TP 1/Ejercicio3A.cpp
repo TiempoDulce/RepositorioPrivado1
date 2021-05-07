fb31f277c437421b506e2af603200b1adfe11d1c
Carlos Diego Simon Hurtado <77213105+TiempoDulce@users.noreply.github.com>
2021-05-06 23:33:35 -0300

    Add files via upload

--- TP 1/Ejercicio3A.cpp
+++ TP 1/Ejercicio3A.cpp
@@ -6,9 +6,17 @@ int main()
 {
     float mtsSeg = 0 ;
     float kmHr = 0;
+
     cout << "Ingrese una velocidad en mts/seg" << endl;
     cin >> mtsSeg;
-    kmHr= mtsSeg * 3.6 ;
-    cout << "Su conversion a kmts/hr es : "<< kmHr << endl;
+
+    if(mtsSeg<0){
+        cout << "Su velocidad ingresada no puede ser negativa" << endl;
+    }
+    else{
+        kmHr= mtsSeg * 3.6 ;
+        cout << "Su conversion a kmts/hr es : "<< kmHr << endl;
+    }
+
     return 0;
 }
--- TP 1/Ejercicio3B.cpp
+++ TP 1/Ejercicio3B.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    float a,b,c,y,x1,x2;
+    float a=0,b=0,c=0,y=0,x1=0,x2=0;
     cout << "Sea su ecuacion de forma \n y = ax^2 + bx + c" << endl;
     cout << "Ingrese y" << endl;
     cin  >> y;
@@ -15,22 +15,28 @@ int main()
     cin  >> b;
     cout << "Ingrese c" << endl;
     cin  >> c;
-    if(y!=0){
-        c = c - y ;
-    }
-    if( ( pow(b,2) - (4*a*c) ) > 0 ){
-        x1= ( -b + pow( ( pow(b,2) - (4*a*c) ) , 0.5 ) ) / (2*a) ;
-        x2= ( -b - pow( ( pow(b,2) - (4*a*c) ) , 0.5 ) ) / (2*a) ;
-        cout << "La primera raiz de la ecuacion de 2do grado es: "<< x1 << endl;
-        cout << "La segunda raiz de la ecuacion de 2do grado es: "<< x2 << endl;
-    }
-    else if( ( pow(b,2) - (4*a*c) ) == 0 ){
-        x1= ( -b + pow( ( pow(b,2) - (4*a*c) ) , 0.5 ) ) / (2*a) ;
-        cout << "La raiz doble de la ecuacion de 2do grado es: "<< x1 << endl;
+    if(a==0){
+        cout << "Su funcion ingresada es una ecuacion lineal"
     }
     else{
-        cout << "La ecuacion no tiene raices reales" << endl;
+        if(y!=0){
+        c = c - y ;
+        }
+        if( ( pow(b,2) - (4*a*c) ) > 0 ){
+            x1= ( -b + pow( ( pow(b,2) - (4*a*c) ) , 0.5 ) ) / (2*a) ;
+            x2= ( -b - pow( ( pow(b,2) - (4*a*c) ) , 0.5 ) ) / (2*a) ;
+            cout << "La primera raiz de la ecuacion de 2do grado es: "<< x1 << endl;
+            cout << "La segunda raiz de la ecuacion de 2do grado es: "<< x2 << endl;
+        }
+        else if( ( pow(b,2) - (4*a*c) ) == 0 ){
+            x1= ( -b + pow( ( pow(b,2) - (4*a*c) ) , 0.5 ) ) / (2*a) ;
+            cout << "La raiz doble de la ecuacion de 2do grado es: "<< x1 << endl;
+        }
+        else{
+            cout << "La ecuacion no tiene raices reales" << endl;
+        }
     }
+
 
     return 0;
 }
